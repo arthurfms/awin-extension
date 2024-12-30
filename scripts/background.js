ba666b99c55c7595b41f87a4a5779fdc750658a2
Arthur Santos <arthurfernandes1993@gmail.com>
2024-12-30 18:06:01 -0300

    Initial data added into the files

---
+++ scripts/background.js
@@ -0,0 +1,20 @@
+chrome.contextMenus.create(
+  {
+    id: "merchant",
+    title: "Go to Merchant: %s",
+    type: "normal",
+    contexts: ["selection"],
+  },
+  () => chrome.runtime.lastError
+);
+
+chrome.contextMenus.onClicked.addListener((item, tab) => {
+  let validatedUrl =
+    /^\d+$/.test(item.selectionText.trim()) &&
+    parseInt(item.selectionText.trim()) > 95
+      ? `https://account.shareasale.com/admin/adminDetailsMerchant.cfm?merchantId=${item.selectionText.trim()}&searchby=${item.selectionText.trim()}`
+      : `https://account.shareasale.com/admin/index.cfm?searchby=${item.selectionText.trim()}&blnUserSearch=1&searchFor=merchants`;
+  let url = new URL(validatedUrl);
+
+  chrome.tabs.create({ url: url.href, index: tab.index + 1 });
+});
